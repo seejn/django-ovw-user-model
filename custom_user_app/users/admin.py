4a72ab28ad8c0c553f9dbfcb335dbf5b244a0eb6
seejn <seejnmaharjan@gmail.com>
2024-05-06 11:33:03 +0545

    created custom UserAdmin for customized admin site

--- custom_user_app/users/admin.py
+++ custom_user_app/users/admin.py
@@ -1,2 +1,27 @@
 from django.contrib import admin
+from .models import Info
+from django.contrib.auth.admin import UserAdmin
 # Register your models here.
+
+
+class AdminSite(UserAdmin):
+    list_display = ("email", "username", "is_staff", "is_active")
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        (("Personal info"), {"fields": ("first_name", "last_name", "email")}),
+        (
+            ("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+
+admin.site.register(Info, AdminSite)
