12d46f6165f86e8dd5482c9241c46269220e5022
seejn <seejnmaharjan@gmail.com>
2024-05-06 12:47:06 +0545

    created models

--- extended_user_primary_field_email/profiles/models.py
+++ extended_user_primary_field_email/profiles/models.py
@@ -1,17 +1,17 @@
 from django.db import models
 from django.utils import timezone
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
 
+from .managers import CustomUserManager
 # Create your models here.
-class Info(models.Model):
-    user = models.OneToOneField(
-        User,
-        on_delete=models.CASCADE
-    )
-    dob = models.DateTimeField()
-    address = models.CharField(
-        max_length=250,
-        null=True,
-        blank=True
-    )
-    phone_number = models.CharField(max_length=25)
+class Info(AbstractUser):
+    email = models.EmailField(max_length=50, unique=True)
+    username = models.CharField(max_length=50, unique=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
