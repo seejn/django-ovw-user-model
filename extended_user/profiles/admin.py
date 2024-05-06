0e896a6e501b4b341d07b50942c1540c9020683e
seejn <seejnmaharjan@gmail.com>
2024-05-06 08:24:31 +0545

    profile registered in admin
    to control from admin site
    - stackedInline to display the profile Info field beneath User field
    - overwriting default admin form view with customized one removing the last_login and date_joined field

--- extended_user/profiles/admin.py
+++ extended_user/profiles/admin.py
@@ -1,4 +1,33 @@
 from django.contrib import admin
-
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
+from .models import Info
 # Register your models here.
 
+class ProfileInline(admin.StackedInline):
+    model=Info
+
+class UserAdmin(UserAdmin):
+    inlines = [ProfileInline]
+
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        (("Personal info"), {"fields": ("first_name", "last_name", "email")}),
+        (
+            ("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+    )
+# unregister old user
+admin.site.unregister(User)
+
+# register new user
+admin.site.register(User, UserAdmin)
