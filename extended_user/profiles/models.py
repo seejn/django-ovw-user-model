cbcb14e0be6e5777d97684e705ffad830ebc8e2d
seejn <seejnmaharjan@gmail.com>
2024-05-06 08:22:10 +0545

    created Info model in profile app
    model to store additional user information
    - user field has onetoone relation with User model
    - User model from django.contrib.auth.models
    - additional field: DOB, address, phone_number

--- extended_user/profiles/models.py
+++ extended_user/profiles/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Info(models.Model):
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE
+    )
+    dob = models.DateTimeField()
+    address = models.CharField(
+        max_length=250,
+        null=True,
+        blank=True
+    )
+    phone_number = models.CharField(max_length=25)
